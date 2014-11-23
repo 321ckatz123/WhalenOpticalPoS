0428a6ca2c4884c3d13eebcd64975c69065d8e10
321ckatz123 <Wizkid$68>
2014-11-23 00:08:04 -0600

    fixing prescription based upon feedback

--- private/js/controllers/IndexCtrl.js
+++ private/js/controllers/IndexCtrl.js
@@ -15,6 +15,7 @@ angular.module('app.controllers')
                 dob: new Date("10/25/1995")
             },
             prescription: {
+                pd: "58",
                 right: {
                     sphere: "-4",
                     cylinder: "-.5",
@@ -37,8 +38,97 @@ angular.module('app.controllers')
             notes: "These are some notes. This is even a lot of notes. These are some notes. " +
             "This is even a lot of notes. These are some notes. This is even a lot of notes. " +
             "These are some notes. This is even a lot of notes. These are some notes. This is even a lot of notes."
-        }];
+        },
+            {
+                id: 2,
+                dateOfSale: new Date("01/01/2014"),
+                information: {
+                    fName: "Trevor",
+                    lName: "Hawke",
+                    address1: "123 Main St.",
+                    address2: "Suite 200",
+                    city: "Hollywood",
+                    state: "CA",
+                    zip: 12345,
+                    phone: "1234567890",
+                    dob: new Date("10/25/1995")
+                },
+                prescription: {
+                    pd: "48",
+                    right: {
+                        sphere: "4",
+                        cylinder: ".5",
+                        axis: "-180",
+                        prism: "-5",
+                        base: "up",
+                        reading: "-9"
+                    },
+                    left: {
+                        sphere: "3",
+                        cylinder: "1",
+                        axis: "270",
+                        prism: "-1.5",
+                        base: "down",
+                        reading: "2"
+                    }
+                },
+                lens: {},
+                frame: {},
+                notes: "These are some notes. This is even a lot of notes. These are some notes. " +
+                "This is even a lot of notes. These are some notes. This is even a lot of notes. " +
+                "These are some notes. This is even a lot of notes. These are some notes. This is even a lot of notes."
+            },
+            {
+                id: 3,
+                dateOfSale: new Date("01/01/2014"),
+                information: {
+                    fName: "Trevor",
+                    lName: "Hawke",
+                    address1: "123 Main St.",
+                    address2: "Suite 200",
+                    city: "Hollywood",
+                    state: "CA",
+                    zip: 12345,
+                    phone: "1234567890",
+                    dob: new Date("10/25/1995")
+                },
+                prescription: {
+                    pd: "48",
+                    right: {
+                        sphere: "4",
+                        cylinder: ".5",
+                        axis: "-180",
+                        prism: "-5",
+                        base: "up",
+                        reading: "-9"
+                    },
+                    left: {
+                        sphere: "3",
+                        cylinder: "1",
+                        axis: "3",
+                        prism: "-1.5",
+                        base: "down",
+                        reading: "2"
+                    }
+                },
+                lens: {},
+                frame: {},
+                notes: "These are some notes. This is even a lot of notes. These are some notes. " +
+                "This is even a lot of notes. These are some notes. This is even a lot of notes. " +
+                "These are some notes. This is even a lot of notes. These are some notes. This is even a lot of notes."
+            }];
 
+        orders = _.sortBy(orders, ['dateOfSale', 'id']).reverse();
         $scope.orders = orders;
-        $scope.mostRecentOrder = _.max(orders, 'id');
+
+        var mostRecentOrder = _.first(orders);
+        var secondMostRecentOrder = _.first(_.at(orders, 1));
+        $scope.viewPriorPrescription = false;
+        $scope.mostRecentAddress = mostRecentOrder.information;
+        $scope.mostRecentPrescription = mostRecentOrder.prescription;
+        $scope.priorPrescription = secondMostRecentOrder ? secondMostRecentOrder.prescription : null;
+
+        $scope.changeCurrentPrescription = function () {
+            $scope.viewPriorPrescription = !$scope.viewPriorPrescription;
+        };
     }]);
